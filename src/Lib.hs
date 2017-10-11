77506a5e19dca74404a3ff944a79881121ad5486
Kuldeep Sengar <sengar.kuldeep@gmail.com>
2017-10-11 22:35:28 +0530

    Added a test case

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    (   factorial,
+        calcBmis,
+        maximum'
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+factorial  :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+calcBmis :: (RealFloat a) => [(a,a)] -> [a]
+calcBmis xs = [bmi w h | (w,h) <- xs]
+        where bmi weight height = weight / height ^ 2
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "Maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Lib
+import Test.HUnit
+import System.Exit
+
+factTest = 5 ~=? factorial 5
+tests = 
+    TestList[
+        TestLabel "factorial" factTest
+    ]
+main = do
+    counts <- runTestTT tests
+    if(errors counts > 0  || failures counts > 0 )
+        then exitFailure
+        else exitSuccess
